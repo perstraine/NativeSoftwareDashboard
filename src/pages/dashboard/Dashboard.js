068b96dab534357d36ae26e7978c6775e5104980
ranjurave <ranjurave@gmail.com>
2022-11-21 14:37:43 +1300

    files organised to folders

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import DashboardHeader from '../../components/dashboardHeader/DashboardHeader';
-import SupportTicketSection from '../../components/SupportTicketSection';
+import SupportTicketSection from '../../components/ZendeskTickets/SupportTicketSection';
 import JiraEpicSection from '../../components/jiraEpics/JiraEpicSection';
 import styles from "./Dashboard.module.css"
 
