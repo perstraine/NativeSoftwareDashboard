ccc4f1c30ee137584882e400ae7df648443b85b6
ranjurave <ranjurave@gmail.com>
2022-11-25 20:16:19 +1300

    new ticket adding from frontend working fine

--- BackEnd/ConsoleUser/Controllers/NewTicketController.cs
+++ BackEnd/ConsoleUser/Controllers/NewTicketController.cs
@@ -19,41 +19,59 @@ namespace ConsoleUser.Controllers
         [HttpPost]
         public async Task<IActionResult> AddTicket(NewTicketFromFrontend newTicketFromFrontend)
         {
-            NewTicket ticketData = CreateNewTicket(newTicketFromFrontend);
             string BasicAuth = "Basic cmFuanVyYXZlZGV2QGdtYWlsLmNvbTpXZWJkZXZfMjAyMg==";
             string Cookie = "__cfruid=ceb12173d3cab3d119fbd3324ebacedd641c6ac7-1669319739; _zendesk_cookie=BAhJIhl7ImRldmljZV90b2tlbnMiOnt9fQY6BkVU--459ed01949a36415c1716b5711271c3d08918307";
+            
             var options = new RestClientOptions("https://native2881.zendesk.com")
             {
                 ThrowOnAnyError = true,
                 MaxTimeout = -1  // 1 second
             };
             var client = new RestClient(options);
+            var usersRequest = new RestRequest("/api/v2/users", Method.Get);
+            usersRequest.AddHeader("Authorization", BasicAuth);
+            usersRequest.AddHeader("Cookie", Cookie);
+            RestResponse usersResponse = await client.ExecuteAsync(usersRequest);
+            var zendeskUsers = JsonConvert.DeserializeObject<ZendeskUsers>(usersResponse.Content);
+
+            NewZendeskTicket.NewTicket ticketToAdd = new NewZendeskTicket.NewTicket();
+            NewZendeskTicket.Ticket ticketData = CreateNewTicket(newTicketFromFrontend, zendeskUsers);
+            ticketToAdd.ticket = ticketData;
 
             var request = new RestRequest("/api/v2/tickets", Method.Post);
             request.AddHeader("Authorization", BasicAuth);
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Cookie", Cookie);
-            request.AddParameter("application/json", ticketData.ticket, ParameterType.RequestBody);
-            RestResponse response = client.Execute(request);
             
-            return Ok(response);
+            request.AddParameter("application/json", ticketToAdd, ParameterType.RequestBody);
+
+            RestResponse response = client.Execute(request);
+
+            return Ok();
         }
 
-        private NewTicket CreateNewTicket(NewTicketFromFrontend? responseData)
+        private NewZendeskTicket.Ticket CreateNewTicket(NewTicketFromFrontend? responseData, ZendeskUsers? zendeskUsers)
         {
-            var billable = new NewTicket.CustomField();
-            billable.id = 12776405299737;
+            NewZendeskTicket.CustomField billable = new NewZendeskTicket.CustomField();
+            billable.id = 12765904262169;
             billable.value = true;
 
-            var newticket = new NewTicket();
-            newticket.ticket.custom_fields = new List<NewTicket.CustomField>();
+            var newticket = new NewZendeskTicket.Ticket();
+            newticket.custom_fields = new List<NewZendeskTicket.CustomField>();
             
-            newticket.ticket.custom_fields.Add(billable);
-            newticket.ticket.type = responseData.type;
-            newticket.ticket.priority = responseData.priority; 
-            newticket.ticket.subject = responseData.subject;
-            newticket.ticket.description = responseData.description;
-            newticket.ticket.requester_id = 12776405299737;
+            newticket.custom_fields.Add(billable);
+            newticket.type = responseData.type;
+            newticket.priority = responseData.priority; 
+            newticket.subject = responseData.subject;
+            newticket.description = responseData.description;
+            foreach (var user in zendeskUsers.users)
+            {
+                if (user.email == responseData.email)
+                {
+                    string tempid = user.id.ToString();
+                    newticket.requester_id = long.Parse(tempid);
+                }
+            }
 
             return (newticket);
         }
--- BackEnd/ConsoleUser/Models/NewTicket.cs
+++ BackEnd/ConsoleUser/Models/NewTicket.cs
@@ -1,22 +1,23 @@
-﻿namespace ConsoleUser.Models
-{ 
+﻿namespace NewZendeskTicket
+{
+    public class CustomField
+    {
+        public long id { get; set; }
+        public bool value { get; set; }
+    }
+
     public class NewTicket
     {
         public Ticket ticket { get; set; }
+    }
 
-        public class CustomField
-        {
-            public long id { get; set; }
-            public bool value { get; set; } = true;
-        }
-        public class Ticket
-        {
-            public string description { get; set; }
-            public string priority { get; set; }
-            public string subject { get; set; }
-            public string type { get; set; }
-            public long requester_id { get; set; }
-            public List<CustomField> custom_fields { get; set; }
-        }
+    public class Ticket
+    {
+        public string priority { get; set; }
+        public string subject { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public long requester_id { get; set; }
+        public List<CustomField> custom_fields { get; set; }
     }
 }
